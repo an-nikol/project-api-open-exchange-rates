c541f5e0b7dd2b66486737a5b469c2f0cb9ea3b6
an-nikol <voynskaa@gmail.com>
2023-11-16 19:00:57 +0200

    updated

--- api_open_exchange_rates.py
+++ api_open_exchange_rates.py
@@ -8,6 +8,7 @@ class Currency:
         self.api_key = open('api_key').readline().strip()
         self.url = f'https://openexchangerates.org/api/latest.json?app_id={self.api_key}'
         self.output = ''
+        self.choice_02 = choice_02
 
     def do_request_01(self):
         response = requests.get(self.url)
@@ -15,19 +16,25 @@ class Currency:
             self.output = response.json()['rates']
             str_response = json.dumps(self.output)
             dict_response = json.loads(str_response)
-            final_list = []
-            for key in dict_response.keys():
+            for key in dict_response:
                 print(key, "     -     ", dict_response[key])
         else:
             return f'Unexpected error. Type of error: {response.status_code}'
 
+    # fix me
     def do_request_02(self):
-        response = requests.get(self.url)
+        response = requests.get(self.url, choice)
         if response.status_code == 200:
             self.output = response.json()['rates']
             str_response = json.dumps(self.output)
             dict_response = json.loads(str_response)
-
+            is_valid_currency = False
+            for curr_symbol in dict_response:
+                if choice_02 == curr_symbol:
+                    is_valid_currency = True
+                    print(f'The current rate for 1 USD is {dict_response[symbol]} {curr_symbol}')
+            if not is_valid_currency:
+                print(f'Please enter a valid currency.')
         else:
             return f'Unexpected error. Type of error: {response.status_code}'
 
@@ -234,7 +241,11 @@ while True:
             print(f'{symbol} ({currency})')
 
         print()
-        chosen_currency = input('Enter one of the currencies to see its corresponding rate per 1 USD')
+        # fix me
+        chosen_currency = input('Enter one of the currencies to see its corresponding rate per 1 USD: ')
+        choice_02 = Currency(chosen_currency)
+        choice_02.do_request_02(chosen_currency)
+
 
     elif choice == 3:
         pass
