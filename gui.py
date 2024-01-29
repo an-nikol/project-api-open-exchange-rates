cf80863d057328ca2dc1026a54c8e7c2a153b0f8
an-nikol <voynskaa@gmail.com>
2024-01-29 23:50:47 +0200

    changed file

--- gui.py
+++ gui.py
@@ -1,38 +1,39 @@
-from tkinter import *
+import tkinter as tk
 
-# create the main window of the app
-root_win = Tk()
-root_win.title('Open Exchange Rates App')
-
-# geometry of the win
-root_win.geometry('800x600')
+window = tk.Tk()
+window.title('Open Exchange Rates App')
+window.geometry('650x450')
 
 menu_text = """Menu
+
 Please choose one of the options below:
 1. All current currency rates
 2. A specific currency rate
-3. Convert from USD to another currency'
+3. Convert from USD to another currency
 4. Statistics for usage
 5. Quit"""
 
-# create a label widget
-center_label = Label(
-    root_win,
+
+center_label = tk. Label(
+    window,
     text=menu_text,
     anchor='e',
     justify='left',
-    # width=70,
     font=('Times New Roman', 15, 'bold')
 )
+center_label.place(x=150, y= 20)
 
-# Pack the label widget to display it
-center_label.pack()
 
-# fix width and height
+input_label = tk.Label(
+    window,
+    text="Enter your choice (1/2/3/4/5): ",
+    font=('Times New Roman', 12, 'bold')
+)
+input_label.place(x=151, y= 242)
+input_field = tk.Entry(window, width=20)
+input_field.place(x=370, y=244)
 
-center_label.place(relx=0.5, rely=0.5, width=100, height=50)
 
-# Run the application
-root_win.mainloop()
 
-# think what is your conception about the app
+
+window.mainloop()
